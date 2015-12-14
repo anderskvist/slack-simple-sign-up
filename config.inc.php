8025405fb68841d5a32b8547cfd4e0f0aab3d70e
Anders Kvist <anderskvist@gmail.com>
2015-12-14 22:06:57 +0100

    Making sure to output errors when in debug mode.

--- config.inc.php
+++ config.inc.php
@@ -4,3 +4,12 @@ $debug = FALSE;
 $logfile = 'debug.log';
 
 $dbfile = 'signup.db';
+
+if ($debug) {
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+} else {
+  error_reporting(E_ALL);
+  ini_set('display_errors', 0);
+}
+
