3f6b410ef0af42cc412bd841b71531b51d70ecf3
Anders Kvist <anderskvist@gmail.com>
2016-01-11 09:36:55 +0100

    Better usage of yargs :)

--- sssu.js
+++ sssu.js
@@ -13,46 +13,44 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+var yargs = require("yargs");
+
 app.post('/', function(req, res){
 
 	var strargv = require("string-argv");
 	var args = strargv.parseArgsStringToArgv(req.body.text);
 
-	var yargs = require("yargs")
+
+	yargs.reset()
 	    .exitProcess(false)
 	    .usage( "Usage: $0 command")
-	    /*	    .command( "attend", "Attend an event")
+	    .command( "attend", "Attend an event")
 	    .command( "create", "Create an event")
 	    .command( "help", "Show this")
 	    .command( "list", "List all open events")
 	    .command( "status", "Status of an event")
 	    .required( 1, "command is required" )
-	    .help('help')
-	    */
-	    .argv,
-	    //	    .parse(args),
-	    command = args[0];
+	    .parse(args);
+
+	var command = args[0];
 
 	if (command == "help") {
 	    res.send(yargs.help());
 	} else if (command == "list") {
 	    database.listEvents(res);
+	} else if (command == "create") {
+	    yargs.reset()
+		.usage(req.body.command + ' create --name "Name of the event" --date "2016-01-01 18:00" --rsvp "2016-01-01 17:00" --note "Some not about this event."')
+		.help('h')
+		.option('name', {alias: 'n', describe: 'Name of the event'})
+		.option('date', {alias: 'd', describe: 'Date and time of the event'})
+		.option('rsvp', {alias: 'r', describe: 'RSVP date and time of the event'})
+		.option('note', {alias: 'r', describe: 'Note for the event'})
+		.parse(args);
+	    res.send("create" + yargs.help());
 	} else {
 	    res.send("Not implemented yet\n\n");
 	}
-
-	if (command == "create") {
-	    //	    var yargs = require("yargs")
-	    //		.usage('$0 create')
-
-	}
-
-	//	console.log(command);
-	console.log(yargs);
-	//	console.log(process.argv);
-
-	yargs.reset;
-	yargs = null;
 });
 
 app.listen(3000);
